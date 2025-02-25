d8eba2e95a30a1e1a5f0cd17fc6676838d403a5a
Riley  Cullen <rileycullen@Rileys-MacBook-Pro.local>
2025-02-24 22:41:18 -0800

    feat: Add ability to render app components

--- src/main-container/container.tsx
+++ src/main-container/container.tsx
@@ -3,7 +3,9 @@ import { AppBar } from './app-bar';
 import { MainContext } from './main-context';
 import { Sidebar } from './sidebar';
 import Box from '@mui/material/Box';
-import { useState, useMemo } from 'react';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import { useState, useMemo, ComponentType, ReactNode } from 'react';
 
 /** Main application container (i.e., entry point to the application).*/
 export function Container() {
@@ -12,16 +14,49 @@ export function Container() {
         () => new ErrorSwallowingAppService(APP_DEFINITIONS),
         []
     );
+    const Component: ComponentType = appService.createComponent(appId)
+        ?? ComponentNotFound;
+
     return (
         <Box>
             <MainContext.Provider
                 value={{
                     appService,
                     currentAppId: appId
-                }}>
-                <AppBar />
-                <Sidebar updateCurrentItem={setAppId} />
+                }}
+            >
+                <Box sx={{ display: 'flex' }}>
+                    <AppBar />
+                    <Sidebar updateCurrentItem={setAppId} />
+                    <AppContainer>
+                        <Component />
+                    </AppContainer>
+                </Box>
             </MainContext.Provider>
+        </Box>
+    );
+}
+
+/** Help component to display when selected app is invalid. */
+function ComponentNotFound() {
+    return (
+        <p>Error: Component not found</p>
+    );
+}
+
+/** Props for {@link AppContainer}. */
+interface AppContainerProps {
+    /** Sub application to render. */
+    children: ReactNode;
+}
+
+/** Container for sub-applications. */
+function AppContainer(props: AppContainerProps) {
+    const { children } = props;
+    return (
+        <Box>
+            <Toolbar />
+            {children}
         </Box>
     );
 }
