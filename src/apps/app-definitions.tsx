f977dd45587ce83724d5eee8bb3b0bc3ee5f8aba
Riley  Cullen <rileycullen@Rileys-MacBook-Pro.local>
2025-03-02 16:45:40 -0800

    Add test board

--- src/apps/app-definitions.tsx
+++ src/apps/app-definitions.tsx
@@ -1,3 +1,4 @@
+import { Board, BoardModel, CellTypes } from '../components';
 import { App } from './app';
 
 export const DEFAULT_APP: App = {
@@ -13,6 +14,45 @@ export const APP_DEFINITIONS: App[] = [
         label: 'Tango Solver',
         component() {
             return <p>Tango!</p>
+        }
+    },
+    {
+        id: 'test-board',
+        label: 'Test Board',
+        component() {
+            const board: BoardModel = {
+                cells: [
+                    [
+                        {
+                            type: CellTypes.String,
+                            id: '00',
+                            value: 'A',
+                            color: ''
+                        },
+                        {
+                            type: CellTypes.String,
+                            id: '01',
+                            value: '',
+                            color: ''
+                        },
+                    ],
+                    [
+                        {
+                            type: CellTypes.String,
+                            id: '10',
+                            value: 'C',
+                            color: ''
+                        },
+                        {
+                            type: CellTypes.String,
+                            id: '11',
+                            value: 'D',
+                            color: ''
+                        },
+                    ]
+                ]
+            };
+            return <Board board={board} />;
         }
     }
 ];
